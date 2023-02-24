f948011c3b4eec6ebd4e2f4da1b7b668a1916cc5
Caroline Gianesini <caroline.gianesini@gmail.com>
2023-02-24 14:28:46 -0300

    exercicio object values

--- fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/exercicio-2/script.js
+++ fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/exercicio-2/script.js
@@ -1,30 +1,38 @@
-const funcionaria = {
-    nome: 'Sara',
-    idade: 40, 
-    função: 'projetista',
-};
-console.log(Object.keys(funcionaria));
+// const funcionaria = {
+//     nome: 'Sara',
+//     idade: 40, 
+//     função: 'projetista',
+// };
+// console.log(Object.keys(funcionaria));
 
-const student1 = {
-    html: 'Muito Bom',
-    css: 'Bom',
-    javascript: 'Ótimo',
-    softskills: 'Ótimo',
-};
+// const student1 = {
+//     html: 'Muito Bom',
+//     css: 'Bom',
+//     javascript: 'Ótimo',
+//     softskills: 'Ótimo',
+// };
   
-const student2 = {
-    html: 'Bom',
-    css: 'Ótimo',
-    javascript: 'Ruim',
-    softskills: 'Ótimo',
-    git: 'Bom',
-};
+// const student2 = {
+//     html: 'Bom',
+//     css: 'Ótimo',
+//     javascript: 'Ruim',
+//     softskills: 'Ótimo',
+//     git: 'Bom',
+// };
 
-function listaSkills(estudante) {
-    const arrayDeSkills = Object.keys(estudante);
-    for (const skill in arrayDeSkills) {
-        console.log(`${arrayDeSkills[skill]}, Nível: ${estudante[arrayDeSkills[skill]]}`);
-    }
-};
+// function listaSkills(estudante) {
+//     const arrayDeSkills = Object.keys(estudante);
+//     for (const skill in arrayDeSkills) {
+//         console.log(`${arrayDeSkills[skill]}, Nível: ${estudante[arrayDeSkills[skill]]}`);
+//     }
+// };
 
-listaSkills(student2);
+// listaSkills(student2);
+
+const funcionaria = {
+    nome: 'Silvana',
+    idade: 45, 
+    função: 'vendedora',
+}
+
+console.log(Object.values(funcionaria));
