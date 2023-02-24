19799af145915582959e55430c80cc679343abf7
Caroline Gianesini <caroline.gianesini@gmail.com>
2023-02-24 14:12:25 -0300

    Exercicio object keys for in

--- fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/exercicio-2/script.js
+++ fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/exercicio-2/script.js
@@ -4,3 +4,27 @@ const funcionaria = {
     função: 'projetista',
 };
 console.log(Object.keys(funcionaria));
+
+const student1 = {
+    html: 'Muito Bom',
+    css: 'Bom',
+    javascript: 'Ótimo',
+    softskills: 'Ótimo',
+};
+  
+const student2 = {
+    html: 'Bom',
+    css: 'Ótimo',
+    javascript: 'Ruim',
+    softskills: 'Ótimo',
+    git: 'Bom',
+};
+
+function listaSkills(estudante) {
+    const arrayDeSkills = Object.keys(estudante);
+    for (const skill in arrayDeSkills) {
+        console.log(`${arrayDeSkills[skill]}, Nível: ${estudante[arrayDeSkills[skill]]}`);
+    }
+};
+
+listaSkills(student2);
