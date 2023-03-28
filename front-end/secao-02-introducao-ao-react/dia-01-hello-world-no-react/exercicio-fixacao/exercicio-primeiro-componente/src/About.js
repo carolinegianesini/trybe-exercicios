2cdf38f3c30529483064287c9e6addd505592efa
Caroline Gianesini <caroline.gianesini@gmail.com>
2023-03-28 20:11:28 -0300

    atualizando pastas

---
+++ front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/exercicio-fixacao/exercicio-primeiro-componente/src/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    {'Caroline Gianesini'}
+                </h1>
+                <p>
+                    {'29 anos, casada, estudante de desenvolvimento web na Trybe'}
+                </p>
+                <h2>Minhas Habilidades</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Java Script</li>
+                    <li>Testes síncronos e assíncronos</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default About;
